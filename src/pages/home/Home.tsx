062ff82ba1f78e988a2ddc336f62d4ce28c9ac06
fernanda <fernandaveeck@gmail.com>
2026-01-21 10:14:09 -0300

    feat: Lista Postagens lógica

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,24 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens";
+
 function Home() {
   return (
     <>
       <main className="bg-indigo-900 flex justify-center">
         <section className="container grid grid-cols-2 text-white">
           <article className="flex flex-col gap-4 items-center justify-center py-4">
-             <h1 className="text-5xl font-bold">Seja Bem Vindo!</h1>
-             <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
-             <button className="flex justify-around gap-4 rounded text-white border-white border-solid border-2 py-2 px-4">
-                Nova Postagem
-                </button>
+            <h1 className="text-5xl font-bold">Seja Bem Vindo!</h1>
+            <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
+            <button className="flex justify-around gap-4 rounded text-white border-white border-solid border-2 py-2 px-4">
+              Nova Postagem
+            </button>
           </article>
           <figure className="flex justify-center w-2/3">
-            <img src="https://i.imgur.com/fyfri1v.png" alt="Imagem página Home"/>
+            <img src="https://i.imgur.com/fyfri1v.png" alt="Imagem página Home" />
           </figure>
         </section>
       </main>
+
+      <ListaPostagens />
     </>
   );
 }
