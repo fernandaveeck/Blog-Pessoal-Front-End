12dcda3df75a003f8908d0f763008130c41e4e24
fernanda <fernandaveeck@gmail.com>
2026-01-22 13:24:20 -0300

    feat: Renderização condicional NavBar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,31 +1,42 @@
-import { useContext } from "react"
+import { ReactNode, useContext } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext"
 
 function Navbar() {
 
     const navigate = useNavigate()
-    const { handleLogout } = useContext(AuthContext)
+    const { usuario, handleLogout } = useContext(AuthContext)
 
     function sair() {
         handleLogout()
+        alert("Logout realizado com sucesso!")
         navigate('/')
     }
+
+    let component: ReactNode
+    if (usuario.token !== undefined) {
+        component = (
+            <>
+                <header className="w-full flex justify-between py-4 bg-indigo-900 text-white">
+                    <nav className="conteiner flex justify-between text-lg mx-8">
+                        <Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link>
+                    </nav>
+                    <menu className="flex gap-6 mx-8">
+                        <Link to='/postagens' className='hover:underline'> <span>Postagens</span> </Link>
+                        <Link to={'/temas'} className="hover:underline"> <span>Temas</span> </Link>
+                        <Link to='/cadastrartema' className='hover:underline'> <span>Cadastrar tema</span> </Link>
+                        <Link to='/perfil' className="hover:underline"> <span>Perfil</span> </Link>
+                        <Link to='' onClick={sair} className="hover:underline"> <span>Sair</span> </Link>
+                    </menu>
+
+                </header>
+            </>
+        )
+    }
+
     return (
         <>
-            <header className="w-full flex justify-between py-4 bg-indigo-900 text-white">
-                <nav className="conteiner flex justify-between text-lg mx-8">
-                    <Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link>
-                </nav>
-                <menu className="flex gap-6 mx-8">
-                    <Link to='/postagens' className='hover:underline'> <span>Postagens</span> </Link>
-                    <Link to={'/temas'} className="hover:underline"> <span>Temas</span> </Link>
-                    <Link to='/cadastrartema' className='hover:underline'> <span>Cadastrar tema</span> </Link>
-                    <Link to='/perfil' className="hover:underline"> <span>Perfil</span> </Link> 
-                    <Link to='' onClick={sair} className="hover:underline"> <span>Sair</span> </Link>
-                </menu>
-
-            </header>
+            {component}
         </>
     )
 }
